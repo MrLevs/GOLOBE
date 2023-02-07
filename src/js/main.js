4c73cffbb4ab37a22707689b2c6094a479df91a2
Stanislav Levanchuk <stanislavlevanchuk@gmail.com>
2023-02-07 22:09:42 +0300

    flight-listing sidebar end

--- src/js/main.js
+++ src/js/main.js
@@ -17,3 +17,36 @@ var mixer = mixitup('.mixitup__box', {
         filter: '.flight'
     }
 });
+//Spoller
+document.querySelectorAll('.spoller__titel').forEach((elem) =>
+    elem.addEventListener('click', () => {
+        elem.classList.toggle('spoller__titel_active');
+        let spollerCont = elem.nextElementSibling;
+        if(spollerCont) {
+            spollerCont.classList.toggle('spoller__content_active');
+        }
+    })
+)
+
+//Slider
+$(".price").ionRangeSlider({
+    type: "double",
+    min: 50,
+    max: 1200,
+    from: 200,
+    to: 500,
+    grid: true,
+    skin: "square",
+    prefix: "$"
+});
+
+$(".time").ionRangeSlider({
+    type: "double",
+    min: 0,
+    max: 24,
+    from: 0,
+    to: 12,
+    grid: true,
+    skin: "square",
+    prefix: ""
+});
